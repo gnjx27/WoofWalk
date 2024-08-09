6cb205abc646902fd6931a6daca7c42ebdb5b684
cpng <ngchunpeng@gmail.com>
2024-08-09 15:35:44 +0800

    update to handle conflict

--- routes/walkerRoutes.js
+++ routes/walkerRoutes.js
@@ -10,17 +10,14 @@ const { getWalkerData, getWalkerReviews, getUserData } = require('../utils/utils
 router.get('/walker-profile', isAuthenticated, async (req, res) => {
     const walkerData = await getWalkerData(global.db, req.session.userId);
     const reviews = await getWalkerReviews(global.db, walkerData.walker_id);
-    const userData = await getUserData(global.db, req.session.userId);
     res.render('index', {
         title: 'Walker Profile - WoofWalk',
         currentPage: 'walker-profile',
         body: 'walker/walker-profile',
         walkerData: walkerData,
-        reviews: reviews,
-        username: userData.username
+        reviews: reviews
     });
 });
-
 
 // Route for editing the walker profile page (protected)
 router.get('/edit-walker-profile', isAuthenticated, async (req, res) => {
@@ -32,6 +29,5 @@ router.get('/edit-walker-profile', isAuthenticated, async (req, res) => {
         walkerData: walkerData
     });
 });
-
 
 module.exports = router;
